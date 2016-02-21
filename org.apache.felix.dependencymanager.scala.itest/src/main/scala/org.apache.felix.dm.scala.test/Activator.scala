4be651d1b4d1ba02cb5f0fac9f332af5b18a9cda
Arjun Panday <arjun.panday@gmail.com>
2016-02-21 23:36:08 +0100

    use my own technique for tests

--- org.apache.felix.dependencymanager.scala.itest/src/main/scala/org.apache.felix.dm.scala.test/Activator.scala
+++ org.apache.felix.dependencymanager.scala.itest/src/main/scala/org.apache.felix.dm.scala.test/Activator.scala
@@ -2,13 +2,20 @@ package org.apache.felix.dm.scala.test
 
 import org.apache.felix.dm.scala.DependencyActivatorBase
 
+import org.scalatest.tools.Runner
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Failure}
+
 class Activator extends DependencyActivatorBase
 {
   def init() = {
+    // initialize some random components
     component(new Comp1()) {
       _.provides(classOf[S1], "name" -> "comp1")
-       .start(_.start)
     }
+
     component(classOf[Comp2]) {
       _.provides(classOf[S2])
        .requires(classOf[S1]) {
@@ -18,14 +25,34 @@ class Activator extends DependencyActivatorBase
        .optionally(classOf[S1])(_.filter("name", "whatever"))
        .start(_.start)
     }
+
+    // inject services to ourself so we can start the actual test
+    component(this) {
+      _.requires(classOf[S1]) {
+         _.added((inst,s) => inst.bind(s))
+       }
+       .requires(classOf[S2]) {
+         _.added((inst,s) => inst.bind(s))
+       }
+       .start(_.start)
+    }
+  }
+
+  def bind(s:S1) = TestDependencies.s1 = Some(s)
+  def bind(s:S2) = TestDependencies.s2 = Some(s)
+
+  def start:Unit = Future {
+    assert( Runner.run(Array("-o",
+      "-u", System.getProperty("test.out"),
+      "-s", classOf[DMSpec].getName)) )
+  } onComplete {
+    case Success(_) => System.exit(0)
+    case Failure(_)  => System.exit(1)
   }
 }
 
 trait S1
 class Comp1 extends S1
-{
-  def start = println("Comp1 started")
-}
 
 trait S2
 class Comp2 extends S2
@@ -38,4 +65,23 @@ class Comp2 extends S2
 
   def start = println("Comp2 started")
 
+}
+
+object TestDependencies
+{
+  var s1:Option[S1] = None //injected by Activator
+  var s2:Option[S2] = None //injected by Activator
+}
+
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class DMSpec extends FlatSpec with Matchers
+{
+  import TestDependencies._
+
+  "DM" should "bind all dependencies" in {
+    s1.get
+    s2.get
+  }
 }
